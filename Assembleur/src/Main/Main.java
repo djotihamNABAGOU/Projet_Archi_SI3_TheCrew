cb5408045f24ef81af6887b1c01d5fd076f9caa0
djotihamNABAGOU <djotiham.nabagou@etu.univ-cotedazur.fr>
2019-01-07 13:41:09 +0100

    Add documentation for prensentation'

--- Assembleur/src/Main/Main.java
+++ Assembleur/src/Main/Main.java
@@ -3,7 +3,6 @@ package Main;
 public class Main {
 
     public static void main(String[] args) {
-        //Engine engine = new Engine("resources/input/input.txt","C://Users/ndaug/Documents/GitHub/Projet_Archi_SI3_TheCrew/Assembleur/resources/output/out.txt");
         Engine engine = new Engine("resources/input/testMax.txt","resources/output/outTestMax.txt");
         engine.run();
     }
